460f1ab33aae482acee474dd2b4b761d57f1e0d8
Marijn Doeve <marijn@doeve.me>
2021-05-14 14:36:46 +0200

    first commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct PokemonListResults: Codable {
+    let results: [PokemonListResult]
+}
+
+struct PokemonListResult: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonResult: Codable {
+    let id: Int
+    let name: String
+    let types: [PokemonTypeEntry]
+    let sprites: PokemonSprites
+}
+
+struct PokemonTypeEntry: Codable {
+    let slot: Int
+    let type: PokemonType
+}
+
+struct PokemonType: Codable {
+    let name: String
+}
+
+struct PokemonSprites: Codable {
+    let backDefault: String
+    let frontDefault: String
+    
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case frontDefault = "front_default"
+    }
+}
